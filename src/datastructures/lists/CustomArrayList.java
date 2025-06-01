025069e13269a950e4efb4cc8ba6043fe595faa6
OiliaO <s.iliarabee2006@gmail.com>
2025-06-01 15:06:13 +0330

    CustomLinkedList class completed

--- src/datastructures/lists/CustomArrayList.java
+++ src/datastructures/lists/CustomArrayList.java
@@ -2,7 +2,7 @@ package datastructures.lists;
 
 import datastructures.interfaces.List;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class CustomArrayList<T> implements List<T> {
     private static final int INITIAL_CAPACITY = 10;
@@ -19,6 +19,78 @@ public class CustomArrayList<T> implements List<T> {
         }
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public boolean add(Object t) {
+        if (t == null) {
+            throw new NullPointerException("Element cannot be null.");
+        }
+        ensureCapacity();
+        elements[size++] = t;
+        return true;
+    }
 
+    @Override
+    public boolean remove(Object o) {
+        if (o == null) {
+            throw new NullPointerException("Element to remove cannot be null.");
+        }
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elements[i])) {
+                removeAtIndex(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private T removeAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        T removedElement = (T) elements[index];
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elements, index + 1, elements, index, numMoved);
+        }
+        elements[--size] = null;
+        return removedElement;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o == null) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elements[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (T) elements[index];
+    }
+
+    @Override
+    public T set(int index, Object element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);}
+        elements[index] = element;
+        return (T) element;}
 }
